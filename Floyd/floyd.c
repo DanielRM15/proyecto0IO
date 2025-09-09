135548c4f42378f9f461a08e792d8add223de645
AdRo0 <adriancoki05@gmail.com>
2025-09-09 11:04:31 -0600

    Added distances input gui

--- Floyd/floyd.c
+++ Floyd/floyd.c
@@ -7,11 +7,47 @@
 GtkWidget *num_nodes_spin;
 GtkWidget *main_stack;
 GtkWidget *distance_table_container;
+GtkWidget *distance_input_grid;
 int nodes = 0;
 
 // Function to create the dynamic distance table
-void create_distance_table() {
-   
+void create_distance_table()
+{
+    char buf[16];
+    for (int i = 0; i <= nodes; i++)
+    {
+        for (int j = 0; j <= nodes; j++)
+        {
+            GtkWidget *to_attach;
+            if (i == 0 && j == 0)
+                continue;
+            if (i == 0 && j != 0)
+            {
+                snprintf(buf, sizeof(buf), "v%d", j);
+                to_attach = gtk_label_new(buf);
+                gtk_grid_attach(GTK_GRID(distance_input_grid), to_attach, j, i, 1, 1);
+                continue;
+            }
+            if (j == 0 && i != 0)
+            {
+                snprintf(buf, sizeof(buf), "v%d", i);
+                to_attach = gtk_label_new(buf);
+                gtk_grid_attach(GTK_GRID(distance_input_grid), to_attach, j, i, 1, 1);
+                continue;
+            }
+            GtkAdjustment *adj = gtk_adjustment_new(0, -1000, 1000, 1, 10, 0);
+            to_attach = gtk_spin_button_new(adj, 1, 0);
+            if (i == j)
+            {
+                gtk_entry_set_text(GTK_ENTRY(to_attach), "0");
+                gtk_widget_set_sensitive(to_attach, FALSE);
+            }
+            else
+                gtk_entry_set_text(GTK_ENTRY(to_attach), "-1");
+            gtk_grid_attach(GTK_GRID(distance_input_grid), to_attach, j, i, 1, 1);
+        }
+    }
+    gtk_widget_show_all(distance_input_grid);
 }
 
 void on_quitBtn_clicked(GtkButton *button, gpointer user_data)
@@ -22,13 +58,14 @@ void on_quitBtn_clicked(GtkButton *button, gpointer user_data)
 void on_continueBtn_clicked(GtkButton *button, gpointer user_data)
 {
     nodes = (int)gtk_spin_button_get_value(GTK_SPIN_BUTTON(num_nodes_spin));
-    if (!nodes) {
+    if (!nodes)
+    {
         return;
     }
-    
+
     // Create the dynamic table
     create_distance_table();
-    
+
     // Switch to the distance input page
     gtk_stack_set_visible_child_name(GTK_STACK(main_stack), "page1");
 }
@@ -42,7 +79,8 @@ int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
 
-    GtkBuilder *builder = gtk_builder_new_from_file("Floyd/floyd.glade");
+    // GtkBuilder *builder = gtk_builder_new_from_file("Floyd/floyd.glade"); //Si se abre desde el menu
+    GtkBuilder *builder = gtk_builder_new_from_file("floyd.glade"); // Si se abre SIN en menu
 
     GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "hWindow"));
 
@@ -54,6 +92,7 @@ int main(int argc, char *argv[])
     main_stack = GTK_WIDGET(gtk_builder_get_object(builder, "mainStack"));
     num_nodes_spin = GTK_WIDGET(gtk_builder_get_object(builder, "numNodesSpin"));
     distance_table_container = GTK_WIDGET(gtk_builder_get_object(builder, "distanceTableContainer"));
+    distance_input_grid = GTK_WIDGET(gtk_builder_get_object(builder, "distance_input_grid"));
 
     g_object_unref(builder);
 
